6b00996eff76abc6dd3206d18943d9df33130fca
Dharmik Mehta <mehtadharmik26101998@gmail.com>
2024-09-14 10:28:44 +0530

    Update script.js - update backend server url

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ async function fetchBookRecommendations() {
       hideError(); // Hide the error message if input is valid
       loader.style.display = 'block';
 
-      const endpoint = 'https://5f24-34-73-121-45.ngrok-free.app/recommendation';
+      const endpoint = 'https://38b7-34-141-247-186.ngrok-free.app/recommendation';
       const requestBody = {
         query: promptInput,
       };
